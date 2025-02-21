9136ba835222f9a6499ea97c936295cbede17b91
humildementebueno <themarshial@gmail.com>
2025-02-21 14:11:36 -0400

    [feat] project init

---
+++ src/main/java/com/labandadeltirador/examen_banco/ExamenBancoApplication.java
@@ -0,0 +1,13 @@
+package com.labandadeltirador.examen_banco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenBancoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenBancoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/labandadeltirador/examen_banco/ExamenBancoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.labandadeltirador.examen_banco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenBancoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
